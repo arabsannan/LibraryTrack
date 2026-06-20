01ea75888a310d872d5e41505482bf644670844e
arabsannan <aaffranannan@gmail.com>
2026-06-20 16:39:37 -0400

    auth views complete - urls added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from accounts import views
+from . import views
 
 app_name = 'accounts'
 
-urlpatterns = []
+urlpatterns = [
+    path("register/", views.RegisterView.as_view(), name="register"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+]
