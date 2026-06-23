4f8ff64afa6ed50b8360c22719c5512b986b83cb
nikhilgirsa <nikhilgirsa@gmail.com>
2026-06-23 12:00:00 -0400

    feat: trim LibraryTrack to dashboard ownership

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,29 +1,3 @@
 from django.contrib import admin
 
-from .models import Book, BookInventory, Library, Notification
-
-
-@admin.register(Library)
-class LibraryAdmin(admin.ModelAdmin):
-	list_display = ("name", "address", "contact_information")
-	search_fields = ("name", "address")
-
-
-@admin.register(Book)
-class BookAdmin(admin.ModelAdmin):
-	list_display = ("title", "isbn", "author", "genre")
-	search_fields = ("title", "isbn", "author", "genre")
-
-
-@admin.register(BookInventory)
-class BookInventoryAdmin(admin.ModelAdmin):
-	list_display = ("book", "library", "total_copies", "available_copies")
-	list_filter = ("library",)
-	search_fields = ("book__title", "book__isbn", "library__name")
-
-
-@admin.register(Notification)
-class NotificationAdmin(admin.ModelAdmin):
-	list_display = ("user", "title", "read_status", "created_at")
-	list_filter = ("read_status",)
-	search_fields = ("user__username", "title", "message")
+# Register your models here.
--- librarytrack/urls.py
+++ librarytrack/urls.py
@@ -19,15 +19,9 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from catalog.views import HomeView
-
 urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('catalog/', include('catalog.urls')),
-    path('borrowing/', include('borrowing.urls')),
     path('dashboard/', include('dashboard.urls')),
-    path('requests/', include('requests.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
